2773cba02e50ae90749452e457e4661aae176c66
Benjamin-Albarzendji <benjamin.albarzendji@gmail.com>
2023-01-24 23:12:28 +0100

    Project set up with Vite, tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      Bitch
+      <div className="text-blue-500 text-blue-500">Lol</div>
+    </div>
+  );
+}
+
+export default App;
